44e6cb3cea7c6f1e3f2eb9a25f0ad4739d474470
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2021-11-30 11:02:21 +0100

    remove collapse and hidden

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 "use strict"
 
+const formDrop = document.querySelector('.menudropdown');
+const bookmark = document.querySelector('.data-actions__add');
+const dataListItem = document.querySelector ('.item__url')
+
+dataListItem.innerHTML += "https://github.com/Mariamafalda2021/Modulo-2-ejercicio-bookmarks"
+
+
+formDrop.classList.remove('collapsed');
+
+bookmark.classList.remove('hidden');
+
